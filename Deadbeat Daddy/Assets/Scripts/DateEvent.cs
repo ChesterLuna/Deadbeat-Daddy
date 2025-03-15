237781aa1f96f90979ed5b47e499efba3d66941d
ChesterLuna <chester99124@gmail.com>
2025-03-15 08:42:52 -0600

    Filled Date Event

--- Deadbeat Daddy/Assets/Scripts/DateEvent.cs
+++ Deadbeat Daddy/Assets/Scripts/DateEvent.cs
@@ -1,7 +1,12 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "DateEvent", menuName = "Scriptable Objects/DateEvent")]
 public class DateEvent : ScriptableObject
 {
-    
+    [SerializeField] public String eventName;
+    [SerializeField] public String description;
+    [SerializeField] public Sprite icon;
+    [SerializeField] public Sprite picture;
+    [SerializeField] public int reward;
 }
