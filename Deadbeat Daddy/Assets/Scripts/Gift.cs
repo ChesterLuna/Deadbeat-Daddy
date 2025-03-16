bcab0fbbbe5a5a0efbee007fb8223268ec942ea4
Max Zang <yzang2@ualberta.ca>
2025-03-15 19:21:46 -0600

    gift random selection

--- Deadbeat Daddy/Assets/Scripts/Gift.cs
+++ Deadbeat Daddy/Assets/Scripts/Gift.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Gift", menuName = "Scriptable Objects/Gift")]
 public class Gift : ScriptableObject
 {
     [SerializeField] public string giftDescription;
+    [SerializeField] public Sprite icon;
+    [SerializeField] public int price;
+    
+
+    void Start()
+    {   
+       
+    }
+
 }
--- Deadbeat Daddy/Assets/Scripts/GiftController.cs
+++ Deadbeat Daddy/Assets/Scripts/GiftController.cs
@@ -1,13 +1,23 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class GiftController : MonoBehaviour
 {   
     private string giftDescription;
+    private Image giftImage;
+    private TMP_Text giftText;
+    private TMP_Text giftPrice;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        giftText = transform.GetChild(0).GetComponent<TMP_Text>();
+        giftImage = GetComponent<Image>();
+        giftPrice = transform.GetChild(1).GetComponent<TMP_Text>();
+
+        DrawGift();
+        Debug.Log("Grabing Gift Description: " + giftDescription);
     }
 
     // Update is called once per frame
@@ -17,13 +27,19 @@ public class GiftController : MonoBehaviour
     }
 
     void DrawGift()
-    {   
+    {
+        Debug.Log("Drawing gift...");
         List<Gift> gifts = GameManager.Instance.gifts;
-        int index = Random.Range(0, gifts.Count);
-        Gift gift = gifts[index];
-        string giftDescription = gifts[index].giftDescription;
-        
+        int randomIndex = Random.Range(0, gifts.Count);
+        Gift randomGift = gifts[randomIndex];
+        giftDescription = randomGift.giftDescription;
+        giftText.text = giftDescription;
 
+        giftImage.sprite = randomGift.icon;
+
+        giftPrice.text = "$ " + randomGift.price.ToString();
+
+        Debug.Log("Gift drawn: " + giftDescription);
     }
     // void DisplayDiscription();
 }
