5b69f301d529563143deef8ddfe847038539086a
ChesterLuna <chester99124@gmail.com>
2025-05-12 23:52:26 -0600

    Spawns cards from top left

--- Deadbeat Daddy/Assets/Scripts/CardSpawner.cs
+++ Deadbeat Daddy/Assets/Scripts/CardSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -20,6 +21,9 @@ public class CardSpawner : MonoBehaviour
 
     [SerializeField] Button drawButton;
 
+    [SerializeField] bool spawnWithAnimation = true;
+    [SerializeField] bool sortingInProgress = false;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -38,6 +42,36 @@ public class CardSpawner : MonoBehaviour
 
     public void SpawnCards(int n)
     {
+        if (spawnWithAnimation)
+        {
+            sortingInProgress = true;
+            StartCoroutine(SpawnCardsWithAnimation(n));
+        }
+        else
+        {
+            nextPoint.position = startPoint.position;
+            for (int i = 0; i < n; i++)
+            {
+                int x_placement = Random.Range(-x_delta, x_delta);
+                int y_placement = Random.Range(-y_delta, y_delta);
+                int rotation_placement = Random.Range(-rotation_delta, rotation_delta);
+
+                Vector3 spawnPoint = nextPoint.position + new Vector3(x_placement, y_placement, 0);
+                SpawnCard(spawnPoint, rotation_placement);
+                nextPoint.localPosition += new Vector3(0, -nextCardMovement, 0);
+
+                if (nextPoint.position.y <= endPoint.position.y)
+                {
+                    nextPoint.position = startPoint.position;
+                }
+                cardsDisplayed++;
+            }
+
+        }
+    }
+
+    IEnumerator SpawnCardsWithAnimation(int n) 
+    {
         nextPoint.position = startPoint.position;
         for (int i = 0; i < n; i++)
         {
@@ -46,23 +80,27 @@ public class CardSpawner : MonoBehaviour
             int rotation_placement = Random.Range(-rotation_delta, rotation_delta);
 
             Vector3 spawnPoint = nextPoint.position + new Vector3(x_placement, y_placement, 0);
-            SpawnCards(spawnPoint, rotation_placement);
-            nextPoint.localPosition += new Vector3(0, -nextCardMovement,0);
+            SpawnCard(spawnPoint, rotation_placement);
+            nextPoint.localPosition += new Vector3(0, -nextCardMovement, 0);
 
-            if(nextPoint.position.y <= endPoint.position.y)
+            if (nextPoint.position.y <= endPoint.position.y)
             {
                 nextPoint.position = startPoint.position;
             }
             cardsDisplayed++;
+            yield return new WaitForSeconds(0.1f);
         }
+        sortingInProgress = false;
     }
 
-    private void SpawnCards(Vector3 pos, int rotation)
+    private void SpawnCard(Vector3 pos, int rotation)
     {
-        GameObject cardSpawned = Instantiate(cardObject, pos, Quaternion.identity, this.transform);
+        GameObject cardSpawned = Instantiate(cardObject, new Vector3(-200,600), Quaternion.identity, this.transform);
         cardSpawned.transform.Rotate(0f, 0f, rotation);
         cardSpawned.transform.SetAsLastSibling();
-        drawButton.targetGraphic = cardSpawned.GetComponent<Image>();
+
+        StartCoroutine(DrawCardAnimation(cardSpawned, pos, 0.5f));
+
     }
 
     public void DrawCard()
@@ -92,4 +130,29 @@ public class CardSpawner : MonoBehaviour
     {
         return transform.GetChild(transform.childCount - 1).gameObject;
     }
+
+
+    IEnumerator DrawCardAnimation(GameObject cardSpawned, Vector3 nextSlot, float time)
+    {
+        Vector3 startingPos = cardSpawned.transform.position;
+        Vector3 finalPos = nextSlot;
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < time)
+        {
+            cardSpawned.transform.position = Vector3.Lerp(startingPos, finalPos, elapsedTime / time);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        // Destroy(cardToPull);
+
+        drawButton.targetGraphic = cardSpawned.GetComponent<Image>();
+
+        // Image nextImage = nextSlot.GetComponent<Image>();
+        // nextImage.sprite = sprite;
+        // Color whiteAlpha = Color.white;
+        // nextImage.color = whiteAlpha;
+    }
+
 }
